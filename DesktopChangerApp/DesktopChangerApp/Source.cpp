543e71c24d838e44e0fa852a77573a144617365d
toby <toby.bell1702@hotmail.co.uk>
2020-06-26 17:20:38 +0100

    Added basic change desktop method

--- DesktopChangerApp/DesktopChangerApp/Source.cpp
+++ DesktopChangerApp/DesktopChangerApp/Source.cpp
@@ -1,10 +1,13 @@
 #include <string>
 #include <iostream>
+#include <Windows.h>
 
 using namespace std; 
 
-int main() {
 
-	cout << "Hello world" << endl;
+int main() {
+	LPCWSTR test = L"C:\\Users\\user\\Pictures\\minion.png";
+	int result = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (LPCWSTR *)test, SPIF_UPDATEINIFILE);
+
 
 }
