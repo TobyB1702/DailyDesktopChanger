0b6a5f79d2005a54f1fead8814db1e21fdd19783
toby <toby.bell1702@hotmail.co.uk>
2020-06-28 15:58:31 +0100

    Added a time computation method test

--- DesktopChangerApp/DesktopChangerApp/Source.cpp
+++ DesktopChangerApp/DesktopChangerApp/Source.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <iostream>
+#include <chrono>
+#include <ctime>   
 #include <Windows.h>
 
 using namespace std; 
@@ -17,8 +19,24 @@ void setWallpaper() {
 
 }
 
+void currentTime() {
+	auto start = std::chrono::system_clock::now();
+	// Some computation here
+	auto end = std::chrono::system_clock::now();
+
+	//chrono::duration<double> elapsed_seconds = end - start;
+	time_t end_time = chrono::system_clock::to_time_t(end);
+
+	cout << end_time << endl;
+
+//	std::cout << "finished computation at " << std::ctime(&end_time)
+//		<< "elapsed time: " << elapsed_seconds.count() << "s\n";
+}
+
 
 int main() {
 	setWallpaper();
+	currentTime();
+
 	return 0;
 }
