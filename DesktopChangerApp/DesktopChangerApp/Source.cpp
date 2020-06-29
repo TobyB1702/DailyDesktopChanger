09a2a7ea4bde63dcb2cdd2e0165c0609d3f2f412
toby <toby.bell1702@hotmail.co.uk>
2020-06-29 14:56:11 +0100

    Restructured file format

--- DesktopChangerApp/DesktopChangerApp/Source.cpp
+++ DesktopChangerApp/DesktopChangerApp/Source.cpp
@@ -7,7 +7,7 @@
 using namespace std; 
 
 void setWallpaper() {
-	string pathwayToImage = "C:\\Users\\tobyb\\source\\repos\\DailyDesktopChanger\\test.JPG";
+	string pathwayToImage = "C:\\Users\\tobyb\\source\\repos\\DailyDesktopChanger\\wallpapers\\1.jpg";
 	
 	int result = SystemParametersInfo(
 		SPI_SETDESKWALLPAPER,
@@ -19,24 +19,21 @@ void setWallpaper() {
 
 }
 
-void currentTime() {
+void computationTime() {
 	auto start = std::chrono::system_clock::now();
 	// Some computation here
 	auto end = std::chrono::system_clock::now();
 
-	//chrono::duration<double> elapsed_seconds = end - start;
 	time_t end_time = chrono::system_clock::to_time_t(end);
 
 	cout << end_time << endl;
 
-//	std::cout << "finished computation at " << std::ctime(&end_time)
-//		<< "elapsed time: " << elapsed_seconds.count() << "s\n";
 }
 
 
 int main() {
 	setWallpaper();
-	currentTime();
+	computationTime();
 
 	return 0;
 }
